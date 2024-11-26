a8cee98e81e058716b50be734e4d4e1d2f588c3a
Panduru Andrei <xxblaxorxx@gmail.com>
2024-11-26 23:56:14 +0200

    Initial Commit

---
+++ MiSWrapperWapp/src/main/java/ro/jdeiutzblaxo/MiSWrapperWapp/MiSWrapperWappApplication.java
@@ -0,0 +1,13 @@
+package ro.jdeiutzblaxo.MiSWrapperWapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiSWrapperWappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiSWrapperWappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/jdeiutzblaxo/MiSWrapperWapp/MiSWrapperWappApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.jdeiutzblaxo.MiSWrapperWapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiSWrapperWappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
